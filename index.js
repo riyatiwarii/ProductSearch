786a392864dd1460b900d53572d8688102109aba
Riya Tiwari <riyatiwari727@gmail.com>
2022-11-16 21:56:47 +0530

    product filter

---
+++ index.js
@@ -0,0 +1,100 @@
+// let productsList = document.querySelector(".product-container")
+
+// fetch('https://fakestoreapi.com/products')
+//             .then(res=>res.json())
+//             .then(json=>{
+//                 addElement(productsList, json)
+//             })
+
+// get value from the api create dynamic element
+
+
+// function addElement(appendIn, value){
+//     let div = document.createElement('div');
+//     div.className = "product_card"
+
+//     let {image, price, title, description} = value
+
+//     div.innerHTML = `
+//     <div class="image_container">
+//     <img
+//       src="${image}"
+//       alt=""/>
+//   </div>
+//   <div class="product_content">
+//     <h2>
+//       ${title}
+//     </h2>
+//     <p>
+//       ${description}
+//     </p>
+//     <button>${price}</button>
+//   </div>
+//     `
+// appendIn.appendChild(div);
+
+// }
+
+
+(async () => {
+    const productContainerEl = document.getElementById("productContainer");
+    const searchInputEl = document.getElementById("searchInput");
+  
+    const url = "https://fakestoreapi.com/products";
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch(url);
+        return await res.json();
+      } catch (error) {
+        return error;
+      }
+    };
+    const products = await fetchProducts();
+  
+    const generateProducts = (product) => {
+      return `<div class="product_card">	
+        <div class="image_container">	
+          <img	
+            src="${product.image}"	
+            alt=""	
+          />	
+        </div>	
+        <div class="product_content">	
+          <h2>	
+           ${product.title}	
+          </h2>	
+          <p>	
+           ${product.description.split(" ").slice(0, 20).join(" ")}	
+          </p>	
+          <button>${product.price} $</button>	
+        </div>	
+      </div>`;
+    };
+  
+    const renderProducts = (products) => {
+      productContainerEl.innerHTML = "";
+      products.forEach((product) => {
+        productContainerEl.innerHTML += generateProducts(product);
+      });
+    };
+  
+    const checkTextContain = (text, searchText) => {
+      return text.toString().toLowerCase().includes(searchText);
+    };
+  
+    const filterHandler = (event) => {
+      const searchText = event.target.value.toLowerCase();
+      const filteredProducts = products.filter((product) => {
+        return (
+          checkTextContain(product.description, searchText) ||
+          checkTextContain(product.title, searchText) ||
+          checkTextContain(product.price, searchText)
+        );
+      });
+      renderProducts(filteredProducts);
+    };
+  
+    searchInputEl.addEventListener("keyup", filterHandler);
+  
+    renderProducts(products);
+  })();
